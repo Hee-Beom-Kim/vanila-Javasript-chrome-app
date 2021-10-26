b0fd947d597b5632233b8a7872e87ee3e758e0af
hbk <hee70b@gmail.com>
2021-10-27 08:52:59 +0900

    Event Listner - default behavior / using css class

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
 
-function onLoginBtnClick(){
-    console.log(loginInput.value);
+function onLoginSubmit(event){
+    event.preventDefault();
+    const userName = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    //const userName = loginInput.value;
+    // console.log(userName);
 }
 
+function onLinkClicked(event){
+    event.preventDefault();
+    console.log(event);
+    // alert("click");
+}
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+
+link.addEventListener("click", onLinkClicked);
