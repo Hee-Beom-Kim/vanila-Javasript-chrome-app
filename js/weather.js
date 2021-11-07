3667c9f634db5f49331b71184f9588cf648a1ca2
hbk <hee70b@gmail.com>
2021-11-08 08:40:55 +0900

    Add Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,16 @@
+const API_KEY = "408e33d24b94df1785f9afeda0a2c6e7";
+
 function onGeoOK(position){
     const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    console.log(`Your current position is latitude: ${latitude}, longitude: ${longitude}`);
+    const longitude = position.coords.longitude;    
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json()).then(data=>{
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;                
+    });
+    
 }
 
 function onGeoError(){
